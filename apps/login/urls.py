20ce1d388f8f518c472df6be92ca5aec7261b18a
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-27 21:35:21 -0500

    moving to ruby

--- apps/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from views import *
-
-urlpatterns = [
-		url(r'^$', index, name='index'),
-    url(r'^login$', authenticate, name='authenticate'),
-    url(r'^register$', register, name='register'),
-    url(r'^logout$', logout, name='logout'),
-]
--- apps/ping_pong/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from views import *
-
-urlpatterns = [
-	url(r'^index$', index, name='index'),
-	url(r'^join_league$', join_league, name='join_league'),
-	url(r'^create_league$', create_league, name='create_league'),
-	url(r'^league/(?P<id>\d+$)',  league, name='league'),
-]
